0b71c7e453af369fd9b99ff722bd2efa4bc82859
selva <selvanithish2014@gmail.com>
2024-09-02 22:13:17 +0530

    Added logo ticker

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import ArrowIcon from "../assets/icons/arrow-w.svg";
+import cursorImage from "../assets/images/cursor.png";
+import messageImage from "../assets/images/message.png";
+import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <div className="bg-black text-white px-[16px] py-[72px] bg-[linear-gradient(to_bottom,#200D42,#200D42_34%,#4F21A1_65%,#A46EDB_82%)]">
-      <div className="">
+    <div className="bg-black text-white px-[16px] py-[72px] sm:py-[96px] bg-[linear-gradient(to_bottom,#200D42,#200D42_34%,#4F21A1_65%,#A46EDB_82%)] overflow-clip relative">
+      <div className="absolute rounded-[100%] h-[375px] sm:w-[1536px] sm:h[768px] w-[750px] lg:w-[2460px] lg:h-[1200px] bg-black left-1/2 -translate-x-1/2 border-[#B48CDE] bg-[radial-gradient(closest-side,#000_82%,#9560EB)] top-[calc(100%-96px)] sm:top-[calc(100%-120px)]"></div>
+      <div className="relative">
         <div className="text-center">
           <a
             href=""
-            className="border border-opacity-30 px-3 py-1 rounded-lg inline-flex items-center space-x-[16px]"
+            className="border border-slate-50/40 px-3 py-1 rounded-lg inline-flex items-center space-x-[16px]"
           >
             <span className="text-transparent bg-clip-text [webkit-background-clip:text] bg-[linear-gradient(to_right,#F87AFF,#FB93D0_25%,#FFDD99_50%,#C3F0B2_75%,#2FD8FE_100%)]">
               Version 2.0 is here
@@ -19,14 +23,29 @@ export const Hero = () => {
             </span>
           </a>
         </div>
-
-        <h1 className="mt-8 text-7xl font-bold text-center tracking-tighter leading-[72px]">
-          One Task at a Time
-        </h1>
-        <p className="text-center leading-[22px] mt-[32px]">
-          Celebrate the joy of accomplishment with an app designed to track your
-          progress, motivate your efforts, and celebrate your successes.
-        </p>
+        <div className="flex justify-center mt-8">
+          <div className="inline-flex relative">
+            <h1 className="mt-8 text-6xl sm:text-9xl font-bold text-center tracking-tighter leading-[72px]">
+              One Task <br></br>at a Time
+            </h1>
+            <Image
+              src={cursorImage}
+              alt=""
+              className="absolute top-[108px] right-[476px] hidden sm:block"
+            />
+            <Image
+              src={messageImage}
+              alt=""
+              className="absolute top-[56px] left-[498px] hidden sm:block"
+            />
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-center leading-[22px] mt-[32px] max-w-md">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress, motivate your efforts, and celebrate your successes.
+          </p>
+        </div>
         <div className="flex justify-center mt-8">
           <button className="px-[20px] bg-white rounded-lg py-[10px] hover:bg-slate-200 transition text-black font-semibold">
             Get for free
--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,19 +1,41 @@
+"use client";
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import Image from "next/image";
 
 const images = [
-  { src: acmeLogo, alt: "Acme Logo" },
-  { src: quantumLogo, alt: "Quantum Logo" },
-  { src: echoLogo, alt: "Echo Logo" },
-  { src: celestialLogo, alt: "Celestial Logo" },
-  { src: pulseLogo, alt: "Pulse Logo" },
-  { src: apexLogo, alt: "Apex Logo" },
+  { src: acmeLogo, alt: "Acme Logo", key: "1" },
+  { src: quantumLogo, alt: "Quantum Logo", key: "2" },
+  { src: echoLogo, alt: "Echo Logo", key: "3" },
+  { src: celestialLogo, alt: "Celestial Logo", key: "4" },
+  { src: pulseLogo, alt: "Pulse Logo", key: "5" },
+  { src: apexLogo, alt: "Apex Logo", key: "6" },
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white">
+      <div className="py-[72px]">
+        <h1 className="text-white/70 text-center text-xl">
+          Trusted by the world's most innovative teams
+        </h1>
+        <div className="pt-[36px]">
+          <div className="flex gap-[64px] overflow-hidden before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-20 after:w-20 relative  before:left-0 after:right-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))]">
+            {images.map(({ src, alt, key }) => (
+              <Image
+                src={src}
+                alt={alt}
+                key={key}
+                className="w-[148px] h-[32px] flex-none"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
